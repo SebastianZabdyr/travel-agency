e56a8986fa33818ad4f43837e3b6bf0b244456da
SebastianZabdyr <you@example.com>
2021-05-31 19:02:19 +0200

    add api server

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,10 +2,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import OrderSummary from '../OrderSummary/OrderSummary.js';
-import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption.js';
+import Button from '../../common/Button/Button';
+import pricing from '../../../data/pricing.json';
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
 
 import {Col} from 'react-flexbox-grid';
+
+
+const sendOrder = (options, tripCost, tripName, tripId, tripCountry) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+    tripName,
+    tripId,
+    tripCountry,
+  };
+
+  if(options.name === '' || options.contact === ''){
+    alert('Wrong name/contact');
+  }
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 class OrderForm extends React.Component {
 
@@ -13,10 +52,13 @@ class OrderForm extends React.Component {
     options: PropTypes.object,
     tripCost: PropTypes.string,
     setOrderOption: PropTypes.func,
+    tripName: PropTypes.string,
+    tripId: PropTypes.string,
+    tripCountry: PropTypes.string,
   }
 
   render(){
-    const {tripCost, options, setOrderOption} = this.props;
+    const {options, tripCost, setOrderOption, tripName, tripId, tripCountry} = this.props;
     return (
       <div>
         {pricing.map(option => (
@@ -27,6 +69,7 @@ class OrderForm extends React.Component {
 
         <Col xs={12}>
           <OrderSummary tripCost={tripCost} options={options} />
+          <Button onClick={() => sendOrder(options, tripCost, tripName, tripId, tripCountry)}>Order now!</Button>
         </Col>
       </div>
     );
